75f06d8807093dfeb195f434108ed581888481ed
Станислав Казаринов <skazarinov@mail.ru>
2025-01-10 12:14:19 +0300

    Шаг 1.01. Роутинг. Добавлены незащищенные маршруты.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,18 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, NotFound404 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
 
 import { AppHeader } from '@components';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path={'/'} element={<ConstructorPage />} />
+      <Route path={'/feed'} element={<Feed />} />
+      <Route path={'*'} element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
