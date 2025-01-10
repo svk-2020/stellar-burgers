0ea8097d11fdbe04864f9e2eafaf4a5a7a8fd7e8
Станислав Казаринов <skazarinov@mail.ru>
2025-01-10 13:41:58 +0300

    Шаг 1.03. Роутинг. Добавлены защищенные маршруты.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,17 +1,76 @@
-import { ConstructorPage, Feed, NotFound404 } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 import { Route, Routes } from 'react-router-dom';
 
 import { AppHeader } from '@components';
+import { ProtectedRoute } from '../protected-route/protected-route';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
     <Routes>
-      <Route path={'/'} element={<ConstructorPage />} />
-      <Route path={'/feed'} element={<Feed />} />
-      <Route path={'*'} element={<NotFound404 />} />
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='*' element={<NotFound404 />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   </div>
 );
