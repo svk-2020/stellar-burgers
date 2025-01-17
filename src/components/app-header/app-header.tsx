f51f73edd2f0b92adb2478ed4343957166fda1f3
Станислав Казаринов <skazarinov@mail.ru>
2025-01-17 13:38:50 +0300

    Шаг 2.2. Тестирование. Чек-лист. Добавлено отображение имени пользователя в шапке сайта

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,9 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../services/store';
+import { selectUserData } from '../../slices/userSlice';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const user = useSelector(selectUserData);
+  return <AppHeaderUI userName={user?.name} />;
+};
